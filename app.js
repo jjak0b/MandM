76ba952efedffbd2d303f3bc25a33eca91a6ee79
Acsor <nildexo@yandex.com>
2020-07-25 21:47:45 +0200

    Improve upon route isolation mechanism
    Modules that now want to define their own routing behavior, can do so
    without considering upper-level app.js file, and targeting the / path
    only.

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var app = express();
 global.__basedir = __dirname;
+
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+/* Dedicate a router to each top-level base path. */
+app.use('/play', require('./routes/player'));
+app.use('/edit', require('./routes/editor'));
+app.use('/scan', require('./routes/qr-scanner'));
+app.use('/shared/*', require('./routes/shared'));
 module.exports = app;
 
-/* each route set its own public folder  */
-app.use(
-    '/player',
-    require('./routes/player')
-);
-app.use(
-	'/editor',
-	require('./routes/editor')
-);
-app.use(
-    '/qr-scanner',
-    require('./routes/qr-scanner')
-);
-app.use(
-	'/shared/*',
-	require('./routes/shared')
-);

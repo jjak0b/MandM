344d8b65bebeeec081f18817e29f0276b4d31af1
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-07-26 00:22:08 +0200

    add vue-i18n plugin add i18n (en,it) front-back cameraType to player CameraWidget

--- player/public/js/CameraWidget.js
+++ player/public/js/CameraWidget.js
@@ -1,8 +1,20 @@
 import QrScanner from '/qr-scanner/qr-scanner.min.js';
 QrScanner.WORKER_PATH = '/qr-scanner/qr-scanner-worker.min.js';
 
+const i18n = new VueI18n({
+	locale: navigator.language,
+	fallbackLocale: 'en',
+	messages: null
+});
+$.get( "locales/"+navigator.language )
+.then( (data) => {
+	console.log( "locales response ", data );
+	if( data ) Object.keys( data ).forEach( locale => i18n.mergeLocaleMessage(locale, data[ locale ] ) );
+});
+
 /* DOM Widget which handle camera, scanner functionalities */
 export var CameraWidget = new Vue({
+	i18n,
 	el: '#app-camera',
 	data: {
 		hasCamera: null,

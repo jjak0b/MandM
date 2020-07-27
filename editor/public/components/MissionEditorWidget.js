bd58f390b357fe9482e232bb451db32995086550
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-07-27 03:00:57 +0200

    Add MissionEditorWidget for title and description (no ready for i18n yet)

--- editor/public/components/MissionEditorWidget.js
+++ editor/public/components/MissionEditorWidget.js
@@ -1,5 +1,63 @@
-import {component} from './MissionEditorWidgetTemplate.js'
+import {template} from "./MissionEditorWidgetTemplate.js";
 
-export var vm = new Vue ({
-	el: "#mission-editor-widget"
+export const component = {
+	template: template,
+	props: {
+		missions: Array,
+		missionId: Number,
+		missionTitle:  String,
+		missionDescription: String
+	},
+	data : function () {
+		return {
+			nextId: 0,
+			missionCache: null
+		}
+	},
+	methods: {
+		save() {
+			let mission = this.missionCache;
+			if( !mission ) {
+				mission = {};
+				this.missions.push( mission );
+				this.missionId = this.nextId ++;
+				console.log( "registered new mission: ", mission );
+			}
+
+			mission.id =  this.missionId;
+			mission.title = this.missionTitle;
+			mission.description = this.missionDescription;
+
+			// clean
+			this.missionId = null;
+			this.missionTitle = null;
+			this.missionDescription = null;
+
+			this.missionCache = null;
+		},
+		remove() {
+			this.missionId = null;
+			this.missionTitle = null;
+			this.missionDescription = null;
+			if( this.missionCache ) {
+				this.missions.splice(this.missions.indexOf(this.missionCache), 1);
+				this.missionCache = null;
+			}
+		},
+		load( mission ) {
+			this.missionCache = mission;
+
+			this.missionId = mission.id;
+			this.missionTitle = mission.title;
+			this.missionDescription = mission.description;
+		}
+	}
+}
+Vue.component( 'mission-editor-widget', component );
+
+export const vm = new Vue ({
+	el: "#mission-editor-widget",
+	data: {
+		missions: []
+	}
 });
--- editor/public/components/MissionEditorWidgetTemplate.js
+++ editor/public/components/MissionEditorWidgetTemplate.js
@@ -1,24 +1,29 @@
 export const template =
-`<div>
-	<label for="title-input">{{ titleLabel }}</label><input type="text" id="title-input">
+`<div class="row">
+	<div class="col-3">
+		<label>Mission list</label>
+		<ul ref="mission-list" class="list-group overflow-auto">
+			<li v-for="mission in missions" v-on:click="load( mission )" class="list-group-item">{{ mission.title }}</li>
+		</ul>
+	</div>
+	<form class="col-9" v-on:submit.prevent="save()" >
+		<div class="form-group">
+			<label for="mission-title">Mission title</label>
+			<input id="mission-title" name="missionTitle" type="text" required="required" class="form-control" v-model="missionTitle" >
+		</div>
+		<div class="form-group">
+			<label for="mission-description"">Mission description</label>
+			<textarea id="mission-description" name="missionDescription" class="form-control" rows="4" v-model="missionDescription"></textarea>
+		</div>
+		<div class="form-group">
+			
+		</div>
+		<div class="form-group btn-group" role="group">
+			<button type="submit" class="form-control btn btn-success">Save</button>
+			<button type="button" v-on:click="remove()" class="form-control btn btn-danger">Delete</button>
+		</div>
+	</form>
 </div>`
 ;
-var localization = {};
-export var component = null;
-
-$.get( `/editor/localization/${navigator.language || navigator.browserLanguage}.json`)
-.then( ( data )=> {
-	localization = data["mission-editor"];
-	console.log( localization  );
-	component = {
-		template: template,
-		props: {
-			"title-label" : String
-		},
-
-	}
-	Vue.component( 'mission-editor-widget', component );
-});
-
 
 

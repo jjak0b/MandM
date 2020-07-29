5b75c85225ccf66825af209e5f197ee96e4e5fad
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-07-29 19:08:26 +0200

    implements MissionEditor form's input using i18n-input-widget

--- editor/public/components/MissionEditorWidget.js
+++ editor/public/components/MissionEditorWidget.js
@@ -1,45 +1,50 @@
 import {template} from "./MissionEditorWidgetTemplate.js";
-import {i18n} from "./Translations.js"
+import {asyncLoad as asyncLoadComponentI18nInputWidget } from "./I18nInputWidget.js";
 
 export const component = {
 	template: template,
 	props: {
 		missions: Array,
-		missionId: Number,
-		missionTitle:  String,
-		missionDescription: String
+		locale: String,
+		localesData: Object
 	},
 	data : function () {
 		return {
 			nextId: 0,
+			missionId: 0,
 			missionCache: null
 		}
+	},
+	components: {
+		'i18n-input-widget': asyncLoadComponentI18nInputWidget
+	},
+	beforeMount: function () {
+
+	},
+	computed: {
+		localeTitle: 		function () {  return Number.isNaN( this.missionId ) ? undefined : 'mission.title.' + this.missionId },
+		localeDescription: 	function () {  return Number.isNaN( this.missionId ) ? undefined : 'mission.description.' + this.missionId }
 	},
 	methods: {
 		save() {
 			let mission = this.missionCache;
 			if( !mission ) {
-				mission = {};
+				mission = [];
 				this.missions.push( mission );
-				this.missionId = this.nextId ++;
+				mission.id = this.nextId++;
+				mission.title = this.localeTitle;
+				mission.description = this.localeDescription;
 				console.log( "registered new mission: ", mission );
 			}
 
-			mission.id =  this.missionId;
-			mission.title = this.missionTitle;
-			mission.description = this.missionDescription;
-
-			// clean
-			this.missionId = null;
-			this.missionTitle = null;
-			this.missionDescription = null;
-
+			// set new Id, so new locale data will be available
 			this.missionCache = null;
+			this.missionId = this.nextId;
+			console.log( "Set new ID: " , this.missionId  );
+
 		},
 		remove() {
-			this.missionId = null;
-			this.missionTitle = null;
-			this.missionDescription = null;
+			this.missionId = this.nextId;
 			if( this.missionCache ) {
 				this.missions.splice(this.missions.indexOf(this.missionCache), 1);
 				this.missionCache = null;
@@ -47,19 +52,7 @@ export const component = {
 		},
 		load( mission ) {
 			this.missionCache = mission;
-
 			this.missionId = mission.id;
-			this.missionTitle = mission.title;
-			this.missionDescription = mission.description;
 		}
 	}
-}
-Vue.component( 'mission-editor-widget', component );
-
-export const vm = new Vue ({
-	el: "#mission-editor-widget",
-	i18n,
-	data: {
-		missions: []
-	}
-});
+};
--- editor/public/components/MissionEditorWidgetTemplate.js
+++ editor/public/components/MissionEditorWidgetTemplate.js
@@ -1,23 +1,48 @@
 export const template =
 `<div class="row" >
 	<div class="col-3">
-		<label>{{ $t( 'MissionEditorWidget.label-mission-list' ) }}</label>
-		<ul ref="mission-list" class="list-group overflow-auto">
-			<li v-for="mission in missions" v-on:click="load( mission )" class="list-group-item list-group-item-action">{{ mission.title }}</li>
+		<label for="mission-list">{{ $t( 'MissionEditorWidget.label-mission-list' ) }}</label>
+		<ul id="mission-list" class="list-group overflow-auto">
+			<li v-for="(mission, index) in missions" v-bind:tabindex='index+1'  v-on:click="load( mission )" v-bind:class="['list-group-item', 'list-group-item-action', missionCache == mission ? 'active' : '' ]">
+				<i18n-input-widget
+					v-bind:tag="'label'"
+					v-bind:locale="locale"
+					v-bind:locales-data="localesData"
+					v-bind:locale-label="mission.title"> 
+				</i18n-input-widget>
+			</li>
 		</ul>
 	</div>
+
 	<form class="col-9" v-on:submit.prevent="save()" >
 		<div class="form-group">
 			<label for="mission-title">{{ $t( 'MissionEditorWidget.label-mission-title' ) }}</label>
-			<input id="mission-title" name="missionTitle" type="text" required="required" class="form-control" v-model="missionTitle" >
+			<i18n-input-widget
+				v-bind:tag="'input'"
+				id="mission-title"
+				name="missionTitle"
+				type="text"
+				required="required"
+				class="form-control"
+				v-bind:locale="locale"
+				v-bind:locales-data="localesData"
+				v-bind:locale-label="localeTitle"></i18n-input-widget>
 		</div>
 		<div class="form-group">
 			<label for="mission-description"">{{ $t( 'MissionEditorWidget.label-mission-description' ) }}</label>
-			<textarea id="mission-description" name="missionDescription" class="form-control" rows="4" v-model="missionDescription"></textarea>
+			<i18n-input-widget
+				v-bind:tag="'textarea'"
+				id="mission-description"
+				name="missionDescription"
+				class="form-control"
+				rows="4"
+				v-bind:locale="locale"
+				v-bind:locales-data="localesData"
+				v-bind:locale-label="localeDescription"></i18n-input-widget>
 		</div>
 		<div class="form-group btn-group" role="group">
-			<button type="submit" class="form-control btn btn-success">{{ $t( 'shared.label-save' ) }}</button>
-			<button type="button" v-on:click="remove()" class="form-control btn btn-danger">{{ $t( 'shared.label-remove' ) }}</button>
+			<button type="submit" class="form-control btn btn-success">{{ missionCache ? $t( 'shared.label-save' ) : $t( 'shared.label-add' ) }}</button>
+			<button type="button" v-on:click="remove()" class="form-control btn btn-danger" v-if="missionCache != null ">{{ $t( 'shared.label-remove' ) }}</button>
 		</div>
 	</form>
 </div>`

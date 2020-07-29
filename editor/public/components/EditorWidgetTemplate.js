23fe0a95489718f6388d26ab6e45b78e0d45d39f
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-07-29 19:10:20 +0200

    Remove testing section in EditorWidget's template

--- editor/public/components/EditorWidgetTemplate.js
+++ editor/public/components/EditorWidgetTemplate.js
@@ -6,24 +6,6 @@ export const template =
 			v-model="locale"></i18n-selector-widget>
 	 </div>
 </section>
-<section id="test">
-	<i18n-input-widget
-			id="mission_title"
-			name="missionTitle"
-			type="text"
-			required="required"
-			class="form-control"
-			v-bind:tag="'input'"
-			v-bind:locale="locale"
-			v-bind:locales-data="localesData"
-			v-bind:locale-label="'MissionEditorWidget.label-mission-title'"></i18n-input-widget>
-	<i18n-input-widget
-			v-bind:tag="'textarea'"
-			class="form-control"
-			v-bind:locale="locale"
-			v-bind:locales-data="localesData"
-			v-bind:locale-label="'MissionEditorWidget.label-mission-description'"></i18n-input-widget>
-</section>
 <section id="story-editor-widget-section" class="tab-pane fade container" role="tabpanel" aria-labelledby="story-tab">
 	<story-editor-widget id="story-editor-widget"></story-editor-widget>
 </section>

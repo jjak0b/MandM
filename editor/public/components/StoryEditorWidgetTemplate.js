4b7539723cc63bd78939b19ca3a44551a075f15e
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-07-30 20:20:21 +0200

    Remove html code leftovers for testing

--- editor/public/components/StoryEditorWidgetTemplate.js
+++ editor/public/components/StoryEditorWidgetTemplate.js
@@ -8,7 +8,6 @@ export const template =
 			</div>
 			<div class="col">
 				<a href="" v-on:click="updateStoryURI( $event )" class="btn">Download</a>
-				<a download="test.png" target="_blank" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" class="btn">Download2</a>
 			</div>
 		</div>
 		<div class="row form-group">

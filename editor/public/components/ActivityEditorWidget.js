81184cb02c43eadde811afae0bc09a09ac2406d3
Lorenzo Castelli <tuonosonante@gmail.com>
2020-08-11 18:57:16 +0200

    Added condition widget component

--- editor/public/components/ActivityEditorWidget.js
+++ editor/public/components/ActivityEditorWidget.js
@@ -1,6 +1,7 @@
 import {template} from "./ActivityEditorWidgetTemplate.js";
 import {component as activityTreeWidgetComponent} from "./ActivityTreeWidget.js";
 import {component as asyncLoadComponentI18nInputWidget} from "./I18nInputWidget.js";
+import {component as conditionActivityOptionWidget} from "./ActivityTypeOpEditorWidget.js";
 import JSTreeNode from "../js/JSTreeNode.js";
 import NodeUtils from "../js/NodeUtils.js";
 import {i18nContent, I18nString} from "./Translations.js";
@@ -31,6 +32,7 @@ export const component = {
 		'activity-tale-editor-widget': activityTaleEditorComponent,
 		'activity-quest-editor-widget': activityQuestEditorComponent,
 		'i18n-input-widget': asyncLoadComponentI18nInputWidget,
+		'option-conditions-widget': conditionActivityOptionWidget,
 		'activity-tree-widget': activityTreeWidgetComponent
 	},
 	data() {

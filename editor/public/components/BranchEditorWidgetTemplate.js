89cf96bc1d650d7cb5801f31fefbba8e843c5057
Lorenzo Castelli <tuonosonante@gmail.com>
2020-08-12 18:22:15 +0200

    Added template for branch editor for every input type (Function one is incomplete)

--- editor/public/components/BranchEditorWidgetTemplate.js
+++ editor/public/components/BranchEditorWidgetTemplate.js
@@ -1,9 +1,31 @@
 export const template = `
  <div>
-<div v-for="(localeLabel, key) in inputType" >
-					<input type="radio" v-bind:id="'mode_'+key" v-bind:value="key" v-model="value.gamemode" name="'gamemode'" aria-describedby="gamemode-description">
-					<label v-bind:for="'mode_' + key">{{ $t(localeLabel + '.label' ) }}</label>
+<div>
+					<select v-for="(valueType, keyType) in inputType">
+					<option v-bind:value="keytype">{{ $t( valueType ) }}</option>
 				</div>
+				<div v-if="keytype==Range">
+				<label for="min">Min</label>
+				<input v-model="min" placeholder="min" id="min">
+				<label for="max">Max</label>
+				<input v-model="max" placeholder="max" id="max">
+</div>
+		<div v-if="keytype==Atom">
+				<input v-model="value">
+				<p>{{ value }}</p>
+				<select name="type">
+				<option>{{ $t( text )}}</option>
+				<option>{{ $t( number )}}</option>
+</select>
+				
+</div>
+		<div v-if="keytype==Function">
+				<select name="type" v-for="function in Functions">
+				<option v-bind="function">{{ $t( function )}}</option>
+				
+</select>
+				
+</div>
  
  
  

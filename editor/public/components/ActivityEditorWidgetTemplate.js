9220edf6d5995ffc65032d83ab503861e9ef5fc5
Lorenzo Castelli <tuonosonante@gmail.com>
2020-08-12 17:12:00 +0200

    Modified tag for branch editor component

--- editor/public/components/ActivityEditorWidgetTemplate.js
+++ editor/public/components/ActivityEditorWidgetTemplate.js
@@ -112,9 +112,9 @@ export const template =
 							</div>
 						</fieldset>
 					</div>
-					<options-condition-widget
+					<branch-editor-widget
 					v-if="this.$refs.treeView.isCondition()">
-					</options-condition-widget>
+					</branch-editor-widget>
 				</div>
 			</form>
 		</div>

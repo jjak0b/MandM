4f598cc3fb73118be9794a17cf1ae2583f0e83da
Lorenzo Castelli <tuonosonante@gmail.com>
2020-08-12 16:57:19 +0200

    Renamed File

--- editor/public/components/BranchEditorWidgetTemplate.js
+++ editor/public/components/BranchEditorWidgetTemplate.js
@@ -1,6 +1,6 @@
 export const template = `
  <div>
-<div v-for="(localeLabel, key) in gametype" >
+<div v-for="(localeLabel, key) in inputType" >
 					<input type="radio" v-bind:id="'mode_'+key" v-bind:value="key" v-model="value.gamemode" name="'gamemode'" aria-describedby="gamemode-description">
 					<label v-bind:for="'mode_' + key">{{ $t(localeLabel + '.label' ) }}</label>
 				</div>

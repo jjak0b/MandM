1aae916bbfac3614babd4760b528ffba2e1c13d6
Lorenzo Castelli <tuonosonante@gmail.com>
2020-08-13 18:43:27 +0200

    Added template with radio inputs

--- editor/public/components/BranchEditorWidgetTemplate.js
+++ editor/public/components/BranchEditorWidgetTemplate.js
@@ -1,27 +1,17 @@
 export const template = `
-<div>
- <div>
-  <select v-for="(valueType, keyType) in inputType">
-    <option v-bind:value="keytype">{{ $t( valueType ) }}</option>
- </div>
- <div v-if="keytype==Range">
-  <label for="min">Min</label>
-  <input v-model="min" placeholder="min" id="min">
-  <label for="max">Max</label>
-  <input v-model="max" placeholder="max" id="max">
- </div>
- <div v-if="keytype==Atom">
-  <input v-model="value">
-  <p>{{ value }}</p>
-  <select name="type">
-   <option>{{ $t( text )}}</option>
-   <option>{{ $t( number )}}</option>
-  </select>
- </div>
-  <div v-if="keytype==Function">
-   <select name="type" v-for="function in Functions">
-	<option v-bind="function">{{ $t( function )}}</option>
-   </select>
- </div>
+<div v-for="(keyType, valueType) in inputTypes" class="form-check">
+								<input
+									type="radio"
+									class="form-check-input"
+									name="inputTy"
+									v-bind:id="'input-type_' + keyType"
+									v-bind:value="valuetype"
+									v-model="value.type"
+								/>
+								<label
+									class="form-check-label"
+									v-bind:for="'input-type_' + type"
+								>{{ $t(localeLabel + '.label' ) }}</label>
+							</div>
 </div> 
 `

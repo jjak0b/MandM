a1dbdbe0db6bd193f9d828f1b68f31d0eca2e73d
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-08-18 01:14:24 +0200

    Add MediaFormWidget to preview (and Add/upload ) media assets to the editor

---
+++ editor/public/components/SceneEditorWidgets/MediaFormWidget.js
@@ -0,0 +1,78 @@
+import { template } from "./MediaFormWidgetTemplate.js";
+import { asyncLoad as asyncLoadComponentI18nInputWidget} from "../I18nInputWidget.js";
+import { asyncLoad as asyncLoadComponentI18nMediaPlayer } from "/shared/components/I18nMediaPlayerWidget.js";
+
+export const component = {
+	template: template,
+	components: {
+		'i18n-input-widget': asyncLoadComponentI18nInputWidget,
+		"i18n-media-player-widget" : asyncLoadComponentI18nMediaPlayer
+	},
+	props: {
+		assetId : Number,
+		locale : String
+	},
+	data() {
+		return {
+			value: null,
+			/*labelMediaSourceTypes : {
+				"file" : "SceneEditor.imgSourceTypes.label_file",
+				"uri" : "SceneEditor.imgSourceTypes.label_uri"
+			},*/
+			labelMediaTypes : {
+				"audio" : "MediaForm.MediaType.label_audio",
+				"video" : "MediaForm.MediaType.label_video",
+				"image" : "MediaForm.MediaType.label_image"
+			},
+			mediaType: null,
+			sourceType: null,
+			files : {
+				main: null,
+				subtitles: {}
+			}
+		}
+	},
+	computed: {
+		localeImageCaptionLabel: function () { return (this.assetId != null && this.assetId != undefined) ? 'assets.caption.' + this.assetId : null },
+	},
+	methods: {
+		shouldPreview() {
+			return this.value && this.value.src;
+		},
+		updateAssetForPreview() {
+			let value = {};
+			value[ "src" ] = this.files.main ? URL.createObjectURL(this.files.main) : null
+			if( this.mediaType == "image" ){
+				value[ "subtitles" ] = this.localeImageCaptionLabel;
+			}
+			else {
+				value[ "subtitles" ] = {};
+				let files = this.files.subtitles;
+				if( files ) {
+					Object.keys( files ).forEach( (locale) => {
+						let binaryData = [];
+						binaryData.push( files[ locale ] );
+						value.subtitles[ locale ] = URL.createObjectURL( new Blob(binaryData, {type: "text/vtt" } ) );
+					});
+				}
+			}
+			this.value = value;
+		},
+		onFileload(event, fileCategory) {
+			let file = event.target.files[0];
+
+			if( fileCategory == "main" ) {
+				this.files.main = file;
+			}
+			else if( fileCategory == "subtitle") {
+				this.files.subtitles[ this.locale ] = file;
+			}
+			else{
+				console.error( "[MediaForm]", "unknown fileCategory",  fileCategory );
+				return;
+			}
+
+			this.updateAssetForPreview();
+		}
+	}
+}
---
+++ editor/public/components/SceneEditorWidgets/MediaFormWidgetTemplate.js
@@ -0,0 +1,138 @@
+export const template =
+`
+<div class="row">
+	<section class="col">
+		<form ref="form" v-on:change="updateAssetForPreview()">
+			<fieldset class="form-group">
+				<legend>{{ $t( "MediaForm.label_mediatype" ) }}</legend>
+				<div v-for="(label, type) in labelMediaTypes" >
+					<input
+						type="radio"
+						name="mediaType"
+						v-bind:id="'mediaForm-media-type_' + type "
+						required="required"
+						v-bind:value="type"
+						v-model="mediaType"
+						v-on:change="$( $refs.form ).trigger('reset')"
+					/>
+					<label
+						v-bind:for="'mediaForm-media-type_' + type "
+					>{{ $t( label ) }}</label>
+				</div>
+			</fieldset>
+				<!-- Only file type is supported for now
+				<fieldset>
+					<legend>{{ $t( "MediaForm.label_mediaSource" ) }}</legend>
+					<div v-for="(label, type) in labelMediaSourceTypes" >
+						<input
+							type="radio"
+							name="mediaSrcType"
+							v-bind:id="'mediaForm-src-type_' + type "
+							required="required"
+							v-bind:value="type"
+							v-model="sourceType"
+						/>
+						<label
+							v-bind:for="'mediaForm-src-type_' + type "
+						>{{ $t( label ) }}</label>
+					</div>
+				</fieldset>
+				-->
+			<fieldset>
+				<legend>{{ $t( "MediaForm.label_upload_file" ) }}</legend>
+				<div v-if="mediaType && mediaType == 'image'">
+					<div class="form-group">
+						<label
+							for="mediaForm-input-file-image"
+						>{{ $t( "MediaForm.label-input-file.image" ) }}</label>
+						<input
+							type="file"
+							name="file"
+							id="mediaForm-input-file-image"
+							required="required"
+							accept="image/*"
+							v-on:change="onFileload($event, 'main')"
+							class="form-control"
+						/>
+					</div>
+					<div class="form-group">
+						<label
+							for="mediaForm-input-image-caption"
+						>{{ $t( "MediaForm.label-input-file.imageCaption" ) }}</label>
+						<i18n-input-widget
+							v-bind:tag="'textarea'"
+							class="form-control"
+							id="mediaForm-input-image-caption"
+							name="fileCaption"
+							rows="4"
+							v-bind:locale="locale"
+							v-bind:locale-label="localeImageCaptionLabel"
+							class="form-control"
+						></i18n-input-widget>
+					</div>
+				</div>
+				<div v-if="mediaType && mediaType == 'video' ">
+					<div class="form-group">
+						<label
+							for="mediaForm-input-file-video"
+						>{{ $t( "MediaForm.label-input-file.video") }}</label>
+						<input
+							type="file"
+							name="file"
+							id="mediaForm-input-file-video"
+							required="required"
+							accept="video/*"
+							v-on:change="onFileload($event, 'main')"
+							class="form-control"
+						/>
+					</div>
+				</div>
+				<div v-if="mediaType && mediaType == 'audio' ">
+					<div class="form-group">
+						<label
+							for="mediaForm-input-file-audio"
+						>{{ $t( "MediaForm.label-input-file.audio") }}</label>
+						<input
+							type="file"
+							name="file"
+							id="mediaForm-input-file-audio"
+							required="required"
+							accept="audio/*"
+							v-on:change="onFileload($event,'main')"
+							class="form-control"
+						/>
+					</div>
+				</div>
+				<div v-if="mediaType && ( mediaType == 'audio' || mediaType == 'video' )" >
+					<div class="form-group">
+						<label
+							for="mediaForm-input-file-subtitles"
+						>{{ $t( "MediaForm.label-input-file.subtitles") }}</label>
+						<input
+							type="file"
+							name="subtitles"
+							id="mediaForm-input-file-subtitles"
+							aria-describedby="mediaForm-input-file-subtitles-description"
+							accept="text/vtt, .vtt"
+							v-on:change="onFileload($event, 'subtitle' )"
+							class="form-control"
+						/>
+					</div>
+					<p id="mediaForm-input-file-subtitles-description">{{ $t( "MediaForm.label-input-file.subtitles_accessibility") }}</p>
+				</div>
+			</fieldset>
+			<div class="btn-group" role="group">
+				<button type="submit" class="form-control btn btn-success">{{ $t("shared.label-add") }}</button>
+				<button type="reset" class="form-control btn btn-danger"></button>
+			</div>
+		</form>
+	</section>
+	<section class="col">
+		<i18n-media-player-widget ref="preview"
+			v-if="shouldPreview()"
+			v-bind:tag="mediaType"
+			v-bind:value="value"
+		></i18n-media-player-widget>
+	</section>
+</div>
+`;

d11ba8c9b519571cec4c720c10add978e7a49745
Lorenzo Castelli <tuonosonante@gmail.com>
2020-08-25 17:51:49 +0200

    Updated FIXME

--- editor/public/components/InputAreaTypeTemplate.js
+++ editor/public/components/InputAreaTypeTemplate.js
@@ -1,13 +1,11 @@
 export const template =
-    //FIXME:Can't display template
+    //FIXME:Can't display template when using v-model=type, gives error
     `
-<div>
 <select v-model="type">
 <option v-bind:value="text">Text</option>
 <option v-bind:value="number">Number</option>
 </select>
-<textarea v-if="type === 'text'" name="txtar" 
-placeholder="Enter text" v-model="svalue"></textarea>
-<input v-if="type === 'number'" name="innum" v-model="svalue">
-</div>
+<!--<textarea v-if="type === 'text'" name="txtar" -->
+<!--placeholder="Enter text" v-model="svalue"></textarea>-->
+<!--<input v-if="type === 'number'" name="innum" v-model="svalue">-->
 `;

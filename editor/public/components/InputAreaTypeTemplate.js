6ab7037eb32b34e2eb1d554f03a04787ba6f1a82
Lorenzo Castelli <tuonosonante@gmail.com>
2020-08-25 16:51:45 +0200

    Added InputAreaTemplate, template doesn't show

---
+++ editor/public/components/InputAreaTypeTemplate.js
@@ -0,0 +1,13 @@
+export const template =
+    //FIXME:Can't display template
+    `
+<div>
+<select v-model="type">
+<option v-bind:value="text">Text</option>
+<option v-bind:value="number">Number</option>
+</select>
+<textarea v-if="type === 'text'" name="txtar" 
+placeholder="Enter text" v-model="svalue"></textarea>
+<input v-if="type === 'number'" name="innum" v-model="svalue">
+</div>
+`;

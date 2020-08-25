3e04c666b17d11678351439ad97a2e441fbe7266
Lorenzo Castelli <tuonosonante@gmail.com>
2020-08-25 17:52:35 +0200

    Added selection input for Function radio button

--- editor/public/components/BranchEditorWidgetTemplate.js
+++ editor/public/components/BranchEditorWidgetTemplate.js
@@ -16,13 +16,16 @@ export const template = `
   >{{ $t(keyType + '.label' ) }}</label>
  </div>
  <div v-if="value === 'Atom'">
-  <textarea name="valuea"></textarea>
-  <lable for="valuea">Valore</lable>
-  <select name="tipo">
-   <option>Number</option>
-   <option>Text</option>
-  </select>
-  <label for="tipo">Type</label>
+ <text-area-input></text-area-input>
+<!--  <textarea v-if="valueAr === 'text'" name="valuea"></textarea>-->
+<!--  <lable v-if="valueAr === 'text'" for="valuea">Text</lable>-->
+<!--  <input type="number" v-if="valueAr === 'number'" name="valuen">-->
+<!--  <label for="valuen">Number</label>-->
+<!--  <label for="tipo">Type</label>-->
+<!--  <select v-model="valueAr" name="tipo">-->
+<!--   <option v-bind:value="number">Number</option>-->
+<!--   <option v-bind:value="text">Text</option>-->
+<!--  </select>-->
  </div>
  <div v-if="value === 'Range'">
     <lable for="min">Min</lable>
@@ -31,13 +34,16 @@ export const template = `
     <textarea id="max"></textarea>
  </div>
  <div v-if="value === 'Function'">
+ <select>
+ <option v-for="func in functionsType">{{func.name}}</option>
+</select>
     <select v-model="valuef">
         <option v-for="(functions, value) in functioVal" 
         v-bind:id="functions"
         v-bind:value="value"> {{ $t( functions + '.label'  ) }}
         </option>
     </select>
-    <div v-if=" valuef === 'Array' ">
+    <div v-if=" valuef === 'Array'">
        <div>
         <textarea v-model="valueAr">
         </textarea> 
@@ -48,14 +54,18 @@ export const template = `
        </div>
        <button v-on:click="save()">Add</button>
     </div>
-    <div v-if=" valuef === 'Value' ">
-        <textarea name="txtFV" placeholder="Insert correlated value..">
-        </textarea>
-        <label for="txtFV">Insertvalue</label>
-        <select>
-            <option>Number</option>
-            <option>Text</option>
+    <div v-if=" valuef === 'Value'">
+    //FIXME:Single value input doesn't show
+       <select v-model="valueAr">
+            <option v-bind:value="Text">text</option>
+            <option v-bind:value="Number">Number</option>
         </select>
+       <div v-if="valuear === 'Text'">
+        <textarea>
+        </textarea> 
+        </div>
+        <div v-if="valuear === 'Number'">
+       </div>
     </div>
     <div v-if=" valuef === 'Variable'">
         <select> 

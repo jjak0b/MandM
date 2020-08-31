6b8fdd11a707320c1d673a58ef8c237a4bd509cc
Lorenzo Castelli <tuonosonante@gmail.com>
2020-08-31 16:07:39 +0200

    Added values variable

--- editor/public/components/InputAreaTypeTemplate.js
+++ editor/public/components/InputAreaTypeTemplate.js
@@ -1,16 +1,14 @@
 export const template = `
 <div>
-       <select v-model="test">
+       <select v-model="inty">
             <option v-bind:value="1">Text</option>
             <option v-bind:value="2">Number</option>
         </select>
         </div>
-        <div v-if="test == 1">
-        <textarea v-model="valuear">
-        </textarea> 
+        <div v-if="type == 1">
+        <input type="text" v-model="value">
        </div>
-       <div v-if="test == 2" min="1">
-       <input type="number" v-model="valuenum">
+       <div v-if="type == 2" min="1">
+       <input type="number" v-model.number="value">
        </div>
-     
 `;
--- editor/public/components/InputAreaTypeWidget.js
+++ editor/public/components/InputAreaTypeWidget.js
@@ -4,12 +4,11 @@ import {template} from "./InputAreaTypeTemplate.js";
 export const component = {
     template:template,
     props:{
-        test:Number
+        inty:Number,
+        values:null
     },
 data(){
         return {
-            valuenum:Number,
-            valuear:String,
         //     type: String,
         //     svalue: {
         //         text:String,
@@ -20,23 +19,5 @@ data(){
                 "numb": "ActivityEditorWidget.input-area.numb"
             }
         }
-},
-    methods:{
-        addT:function(){
-            if(this.value){
-                if(this.type === 'text'){
-                    this.list.push(this.value.txt);
-                }
-            }
-        },
-        addN:function(){
-            if(this.value){
-                if(this.type === 'number'){
-                    this.list.push(this.value.num);
-                }
-            }
-        }
-    }
-
-
+}
 }

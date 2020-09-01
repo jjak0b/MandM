d90c497e3df767891f50b144c31c629fa1b09803
Lorenzo Castelli <tuonosonante@gmail.com>
2020-09-01 19:08:51 +0200

    inty replaced with type and relocated in data

--- editor/public/components/InputAreaTypeWidget.js
+++ editor/public/components/InputAreaTypeWidget.js
@@ -4,16 +4,11 @@ import {template} from "./InputAreaTypeTemplate.js";
 export const component = {
     template:template,
     props:{
-        inty:Number,
         values:null
     },
 data(){
         return {
-        //     type: String,
-        //     svalue: {
-        //         text:String,
-        //         num:Number
-        //     },
+            type:Number,
             inputT: {
                 "text": "ActivityEditorWidget.input-area.text",
                 "numb": "ActivityEditorWidget.input-area.numb"

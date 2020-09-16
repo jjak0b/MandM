cf4b367314814ac5fe003d1009fc5cc4fd434ef2
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-09-16 16:46:18 +0200

    [StoryEditor] Add Form to create and put a new story to server; Keep trying to get story names until they're updated if any error happen during server fetch

--- editor/public/components/StoryEditorWidgetTemplate.js
+++ editor/public/components/StoryEditorWidgetTemplate.js
@@ -1,26 +1,62 @@
 export const template =
 `<div>
-	<form v-on:submit.prevent >
-		<div class="row form-group">
-			<div class="col">
-				<input type="file" id="input-file" accept="application/json" v-on:change="onFileload($event)">
-				<!--<label for="input-file"></label>-->
-			</div>
-			<div class="col">
-				<a href="" v-on:click="updateStoryURI( $event )" class="btn">Download</a>
-			</div>
-		</div>
-		<div class="row form-group">
-			<div class="col">
-				<div v-for="(localeLabel, key) in gamemodes" >
-					<input type="radio" v-bind:id="'mode_'+key" v-bind:value="key" v-model="value.gamemode" name="'gamemode'" aria-describedby="gamemode-description">
-					<label v-bind:for="'mode_' + key">{{ $t(localeLabel + '.label' ) }}</label>
-				</div>
-			</div>
-			<div class="col" id="gamemode-description">
-				<p v-for="(localeLabel, key) in gamemodes" v-if="value.gamemode == key">{{ $t( localeLabel + '.description' ) }}</p>
-			</div>
-		</div>
-	</form>
+	<b-form-file
+		v-on:change="onFileload"
+		accept="application/json"
+		name="data"
+	>
+	</b-form-file>
+	<a href="" v-on:click="updateStoryURI( $event )" class="btn">Download</a>
+	<b-form
+		v-on:submit.prevent="addStoryRemote"
+	>
+		<b-form-row>
+			<b-col>
+				<b-form-group
+					label-for="story-editor-widget-form-create-input-name"
+					v-bind:label="$t('StoryEditor.label-story-name')"
+					v-bind:state="stateNewStory"
+				>
+					<template v-slot:description
+					><h4>{{ $t('StoryEditor.label-create-new-story') }}</h4></template>
+					<template v-slot:valid-feedback>
+						<span v-t="'StoryEditor.label-valid-name-available'"></span>
+					</template>
+					<template v-slot:invalid-feedback
+					>
+						<span v-if="!tmpStory.name" v-t="'StoryEditor.label-invalid-name-is-required'"></span>
+						<span v-else v-t="'StoryEditor.label-invalid-name-already-exists'"></span>
+					</template>
+					<b-form-input
+						id="story-editor-widget-form-create-input-name"
+						required="required"
+						v-model="tmpStory.name"
+						name="name"
+					></b-form-input>
+				</b-form-group>
+			</b-col>
+		</b-form-row>
+		<input
+			type="submit"
+			class="btn btn-success"
+		/>
+	</b-form>
+	<b-form
+		v-on:submit.prevent
+	>
+		<b-form-group>
+			<b-form-row>
+				<b-col>
+					<div v-for="(localeLabel, key) in gamemodes" >
+						<input type="radio" v-bind:id="'mode_'+key" v-bind:value="key" v-model="value.gamemode" name="'gamemode'" aria-describedby="gamemode-description">
+						<label v-bind:for="'mode_' + key">{{ $t(localeLabel + '.label' ) }}</label>
+					</div>
+				</b-col>
+				<b-col id="gamemode-description">
+					<p v-for="(localeLabel, key) in gamemodes" v-if="value.gamemode == key">{{ $t( localeLabel + '.description' ) }}</p>
+				</b-col>
+			</b-form-row>
+		</b-form-group>
+	</b-form>
 </div>`
 ;

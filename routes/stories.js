3419c0e649003df93b8ba250bf1f85d02910be68
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-09-16 20:20:47 +0200

    [StoryEditor/Server] Add REST PUT and GET at url "/stories" - method: PUT: "/stories" query : name description: create/replace a story with name body: JSON response: JSON ; method:GET "/stories/[name]" parameter : name description: return list names of stored stories; if name param defined then return JSON story data response: JSON

---
+++ routes/stories.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const fs = require('fs');
+
+const dirNameStories = "stories"
+const pathFolderStories = path.join( __basedir, dirNameStories );
+var storiesSaved = [];
+
+function updateStoryNames() {
+	if ( !fs.existsSync(pathFolderStories) ){
+		fs.mkdirSync( pathFolderStories.toString() );
+	}
+	else{
+		console.log("Detecting stories ..." );
+		fs.readdir( pathFolderStories.toString(), (err, filenames ) => {
+			if( err ){
+				console.error("Error on reading folder: ", `"${pathFolderStories}"`, err);
+			}
+			else{
+				filenames.forEach( (name) => {
+					if( !storiesSaved.includes( name ) ) storiesSaved.push( name );
+				});
+				console.log("Stories detected: ", storiesSaved );
+			}
+		});
+	}
+}
+
+updateStoryNames();
+
+router.get('/', ( req, res ) => {
+	res.json( storiesSaved );
+});
+
+router.get('/:name', ( req, res ) => {
+	if( req.params.name ) {
+		let storyDir = path.join( __basedir, dirNameStories, req.params.name);
+
+		fs.readFile( path.join( storyDir, "story.json" ), 'utf8', function (err, data) {
+			if (err) throw err;
+			else{
+				res.json( data );
+			}
+		});
+	}
+});
+
+router.put("/", ( req, res ) => {
+
+	let storyDir = path.join( __basedir, dirNameStories, req.query.name);
+
+	if ( !fs.existsSync( storyDir ) ){
+		fs.mkdirSync( storyDir );
+	}
+
+	fs.writeFile( path.join( storyDir, "story.json" ), JSON.stringify(req.body), 'utf8', function (err) {
+		if (err) throw err;
+		else{
+			storiesSaved.push( req.query.name );
+			console.log("Story added: ", req.query.name );
+			res.end('{"status" : 200}');
+		}
+	});
+});
+
+module.exports = router;

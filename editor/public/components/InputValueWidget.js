0f7b29ff47b3dfa29666417b841dc4347dec3e23
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-09-28 22:39:52 +0200

    [InputValue] Changes to InputValue to improve UI and props interface

--- editor/public/components/InputValueWidget.js
+++ editor/public/components/InputValueWidget.js
@@ -1,65 +1,178 @@
-import {template} from "./InputValueWidgetTemplate.js";
-// import {component as datePickerEditorComponent} from "datePickerEditorComponent"
+import {template, templateArray} from "./InputValueWidgetTemplate.js";
 
-export const component = {
-    template: template,
-    components:{
-        // "user-widget-editor-datepicker": datePickerEditorComponent
+export var  component = null,
+            componentArray = null;
+
+componentArray = {
+    inheritAttrs: false,
+    template: templateArray,
+    components: {
+
+    },
+    props: {
+        value: Array,
     },
     data() {
         return {
-            componentsTag: {
-                Time: {
-                    name:"b-time",
-                    attrs:"time"
-                },
-                Date: {
-                    name:"b-form-input",
-                    attrs:"date"
-                },
-                Text: {
-                   name: "b-form-input",
-                    attrs:"text"
-                },
-                Number:{
-                    name:"b-form-input",
-                attrs: "number"
-            }
-            },
-            type: '',
-            param: [],
-            arrayType:'',
-            temp:null
+            type: null,
+            tmpValue: null,
+            buffer: [],
+            selectedIndex: 0
         }
     },
-    watch:{
-        'selectType': function(){
-            $emit('taketype', type);
+    computed: {
+        bufferOptions: function () {
+            let array = new Array( this.buffer.length );
+            this.buffer.forEach( (value, i) => {
+                array[ i ] = {
+                    value: i,
+                    text: value
+                }
+            });
+            return array;
+        }
+    },
+    watch: {
+        "value": function (newVal) {
+            if( newVal )
+                this.buffer = newVal;
+            else
+                this.buffer = [];
+            this.selectedIndex = null;
         },
-      'type': function(){
-          this.wipe();
-      }
+        "type": function (newVal) {
+            this.wipeBuffer();
+        }
+    },
+    methods: {
+        add() {
+            this.buffer.push( this.tmpValue );
+            this.$emit('input', this.buffer );
+        },
+        move( index, offset ) {
+            let newIndex = index + offset;
+            if( 0 <= newIndex && newIndex < this.buffer.length ) {
+                this.buffer.splice( newIndex, 0, this.buffer.splice( index, 1 )[0] );
+            }
+        },
+        wipeBuffer() {
+            this.selectedIndex = null;
+            while( this.buffer && this.buffer.length > 0 )
+                this.buffer.pop();
+        }
+    }
+};
+
+component = {
+    template: template,
+    components:{
+    },
+    props: {
+        value: Object,
+        acceptTypes: {
+            type: Array,
+            default: [
+                "Time",
+                "Date",
+                "Text",
+                "Number",
+                "Array"
+            ]
+        },
+        context: Object
+    },
+    data() {
+        return {
+            componentContext: {
+                Time: {
+                    component :"b-time",
+                    attrs: {
+                        type: "time"
+                    },
+                    i18n: {
+                        type: 'shared.label-Time',
+                        description: "ActivityEditorWidget.tnt-desc"
+                    }
+                },
+                Date: {
+                    component: "b-datepicker",
+                    attrs: {
+                        type: "date"
+                    },
+                    i18n: {
+                        type: 'shared.label-Date',
+                        description: "ActivityEditorWidget.tnt-desc"
+                    }
+                },
+                Text: {
+                    component: "b-form-input",
+                    attrs: {
+                        type: "text"
+                    },
+                    i18n: {
+                        type: "shared.label-Text",
+                        description: "ActivityEditorWidget.tnt-desc"
+                    }
+                },
+                Number:{
+                    component:"b-form-input",
+                    attrs: {
+                        type: "number"
+                    },
+                    i18n: {
+                        type: "shared.label-Number",
+                        description: "ActivityEditorWidget.tnt-desc"
+                    }
+                },
+                Array: {
+                    component: "input-array",
+                    i18n: {
+                        type: "shared.label-Array",
+                        description: "ActivityEditorWidget.tnt-desc"
+                    }
+                }
+            },
+            typedValue: {
+                type: null,
+                value: null
+            }
+        }
+    },
+    created() {
+        // merge context
+        if( this.context ) {
+            this.componentContext = Object.assign( this.componentContext, this.context );
+        }
+    },
+    computed: {
+        componentDataForType: function () {
+            return ( this.typedValue.type && this.componentContext[ this.typedValue.type ] ) ? this.componentContext[ this.typedValue.type ] : null;
+        }
+    },
+    watch: {
+        'typedValue.type': function(newVal, oldVal){
+            if( newVal != oldVal ){
+                this.$emit('taketype', newVal);
+                this.wipe();
+            }
+        },
+        'typedValue.value': function(newVal, oldVal){
+            console.log("emitting", this.typedValue );
+            this.$emit( 'input', this.typedValue );
+        },
+        "value": function (newVal) {
+            this.$set( this.typedValue, "type", newVal.type );
+            this.$set( this.typedValue, "value", newVal.value );
+        }
     },
     methods:{
-        check: function () {
-            this.param.pop();
-            this.param.push(this.temp);
-        },
-        wipe:function (){
-            var i=0;
-            while(i<this.param.length){
-                this.param.pop();
-            }
-        },
-        rem: function(){
-            var position=this.param.indexOf(this.temp,0);
-            if(position != -1) {
-                this.param.splice(position, 1);
-            while (position < this.param.lenght) {
-                this.param[position] = this.param[position + 1];
-                position++;
-            }
-            }
+
+        wipe:function () {
+            this.$set( this.typedValue, "value", null );
+            this.$emit( 'input', this.typedValue );
         }
     }
 }
+
+componentArray.components[ "input-value-widget" ] = component;
+component.components[ "input-array" ] = componentArray;
--- editor/public/components/InputValueWidgetTemplate.js
+++ editor/public/components/InputValueWidgetTemplate.js
@@ -1,59 +1,138 @@
 export const template =
-    `
-<div>
-    <select id="selectType" v-model="type" v-on:change="$emit('taketype',type)">
-        <option value="Text">{{ $t('shared.label-Text') }}</option>
-        <option value="Number">{{ $t('shared.label-Number') }}</option>
-        <option value="Array">{{ $t('shared.label-Array') }}</option>
-        <option value="Time">{{ $t('shared.label-Time') }}</option>
-        <option value="Date">{{$t('shared.label-Date')}}</option>
-    </select>
-    <div v-if="type == 'Number'" aria-describedby="Num">
-    <b-alert show id="Num">{{ $tc('ActivityEditorWidget.tnt-desc', 'shared.label-Number') }}</b-alert>
-        <input type="number" v-model.number="temp" aria-describedby="Number">
-        <button v-on:click="check()">{{ $t('shared.label-add') }}</button>
-    </div>
-    <div v-else-if="type == 'Text'" aria-describedby="Tex">
-    <b-alert show id="Tex">{{ $tc('ActivityEditorWidget.tnt-desc', 'shared.label-Text') }}</b-alert>
-        <input type="text" v-model="temp">
-        <button v-on:click="check()" v-on:submit.prevent>{{ $t('shared.label-add') }}</button>
-    </div>
-    <div v-else-if="type == 'Array'" aria-describedby="Ar">
-    <b-alert show id="Ar">{{ $t('ActivityEditorWidget.array-desc') }}</b-alert>
-    <select v-model="arrayType">
-    <option v-for="(comp, type) in componentsTag"
-    v-bind:value="comp">
-    {{$t('shared.label-'+ type)}}
-</option>
-</select>
-<component
-  :is="arrayType.name"
-  v-bind:type="arrayType.attrs"
-    v-model="temp"
-></component>
-<!--   ? componentsTag[arrayType].attrs:{}-->
-    <button v-on:click="param.push(temp)"v-on:submit.prevent>{{ $t('shared.label-add') }}</button>
-    <button v-on:click="rem()" v-on:submit.prevent>{{ $t('shared.label-remove') }}</button>
-<!--    <component v-if="arrayType"-->
-<!--    v-bind:is="componentsType[arrayType]"-->
-<!--    v-model="temp">-->
-<!--    </component>-->
-    </div> 
-    <!--TODO: Need to add locale attribute for time, based on the value in 18 translations--> 
-    <div v-else-if="type == 'Time'" aria-describedby="Ti" locale="locale">
-    <b-alert show id="Ti">{{ $tc('ActivityEditorWidget.tnt-desc', 'shared.label-Time') }}</b-alert>
-    <b-time v-model="temp"></b-time>
-     <button v-on:click="check()" v-on:submit.prevent>{{ $t('shared.label-add') }}</button>
-</div>
-<div v-else-if="type == 'Date'" aria-describedby="Da">
-    <b-alert show id="Da">{{ $tc('ActivityEditorWidget.tnt-desc', 'shared.label-Date') }}</b-alert>
-        <input type="date" aria-describedby="Da" v-model="temp">
-        <button v-on:click="check()">{{ $t('shared.label-add') }}</button>
-    </div>
-    <button v-on:click="$emit('value', param)">{{ $t('shared.label-save') }}</button> 
-    <div v-for="val in param">
-        {{val}}
-    </div>
-    
-</div>
+`
+<form
+	v-bind:aria-labelledby="$attrs.id + '_data-editor-widget-legend'"
+>
+	<fieldset>
+		<legend
+			v-bind:id="$attrs.id + '_data-editor-widget-legend'"
+			v-t="'DataEditorWidget.label-create-value-you-need'"
+		></legend>
+		
+		<b-form-group
+			v-bind:label="$t('DataEditorWidget.label-value-type')"
+			v-bind:label-for="$attrs.id + '_data-editor-widget-select-type'"
+		>
+			<select
+				v-bind:id="$attrs.id + '_data-editor-widget-select-type'"
+				v-model="typedValue.type"
+				class="form-group"
+			>
+				<option
+					v-for="type in acceptTypes"
+					v-bind:value="type"
+					v-t="componentContext[ type ].i18n.type"
+				></option>
+			</select>
+		</b-form-group>
+		
+		<b-form-group
+			v-if="typedValue.type"
+			v-bind:label="componentDataForType.i18n ? $t( componentDataForType.i18n.type ) : typedValue.type"
+			v-bind:label-for="$attrs.id + '_data-editor-widget-atom-' + typedValue.type"
+			v-bind:description="$tc( componentDataForType.i18n.description, componentDataForType.i18n.type )"
+		>
+			<component
+				v-if="componentDataForType"
+				v-bind:key="'atom-' + typedValue.type"
+				v-bind:id="$attrs.id + '_data-editor-widget-atom-' + typedValue.type"
+				v-bind:is="componentDataForType.component"
+				v-bind="componentDataForType.attrs"
+				v-model="typedValue.value"
+			></component>
+		</b-form-group>
+	</fieldset>
+</form>
 `;
+
+export const templateArray =
+`
+<b-form>
+	<fieldset>
+		<legend v-t="'shared.label-Array-info'"></legend>
+		<b-form-row>
+			<b-col>
+		
+				<input-value-widget
+					v-on:taketype="type = $event"
+					v-on:input="tmpValue = $event.value"
+				>
+				</input-value-widget>
+				<b-button
+					v-bind:disabled="tmpValue == null"
+					variant="primary"
+					v-t="'shared.label-add'"
+					v-on:click="add"
+				></b-button>
+			</b-col>
+			<b-col>
+				
+				<label
+					v-bind:id="$attrs.id + '_data-editor-widget-array-values'"
+					v-t="'DataEditorWidget.label-values'"
+				></label>
+				<b-row no-gutters>
+					<b-col>
+						<b-form-select
+							v-bind:id="$attrs.id"
+							v-bind:aria-describedby="$attrs.id + '_data-editor-widget-array-values'"
+							v-model="selectedIndex"
+							v-bind:options="bufferOptions"
+							select-size="6"
+						></b-form-select>
+					</b-col>
+					<b-col class="align-self-center">
+						<b-button-group v-if="selectedIndex != null"
+							tag="span"
+							v-bind:aria-controls="$attrs.id"
+							class="d-flex justify-content-around"
+						>
+							<b-button-group
+								size="sm"
+								vertical
+								tag="span"
+							>
+								<b-button
+									variant="secondary"
+									v-bind:title="$t('shared.label-move-before')"
+									v-bind:aria-label="$t('shared.label-move-before')"
+									v-bind:disabled="selectedIndex <= 0"
+									v-on:click="move( selectedIndex, -1 )"
+								>
+									<span
+										class="glyphicon glyphicon-arrow-up"
+										role="img"
+										aria-hidden="true"
+									></span>
+								</b-button>
+								
+								<b-button
+									variant="secondary"
+									v-bind:title="$t('shared.label-move-after')"
+									v-bind:aria-label="$t('shared.label-move-after')"
+									v-bind:disabled="selectedIndex >= buffer.length - 1"
+									v-on:click="move( selectedIndex, 1 )"	
+								>
+									<span
+										class="glyphicon glyphicon-arrow-down"
+										role="img"
+										v-bind:title="$t('shared.label-move-after')"
+										aria-hidden="true"
+									></span>
+								</b-button>
+							</b-button-group>
+							
+							<span class="my-auto">
+								<b-button
+									variant="danger"
+									v-on:click="buffer.splice( selectedIndex, 1)"
+								>{{ $t('shared.label-remove') }}</b-button>
+							</span>
+						</b-button-group>
+					</b-col>
+				</b-row>
+			</b-col>
+		</b-form-row>
+	</fieldset>
+</b-form>
+`

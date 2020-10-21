dab235c57cb6d07a762bf2d738a8f3af8050808a
Iacopo <i.porcedda2@gmail.com>
2020-10-21 17:08:18 +0200

    [SceneEditor] Add translations for UserWidgetTextInput

--- editor/public/components/SceneEditorWidgets/UserWidgetEditors/UserWidgetEditorTextInput.js
+++ editor/public/components/SceneEditorWidgets/UserWidgetEditors/UserWidgetEditorTextInput.js
@@ -1,13 +1,26 @@
 import {template} from "./UserWidgetEditorTextInputTemplate.js";
 import {component as textInputComponent} from "/shared/components/UserWidgetTextInput.js";
+import {component as asyncLoadComponentI18nInputWidget} from "../../i18nWidgets/I18nInputWidget.js";
 
 export const component = {
 	template: template,
 	props: {
 		props: Object,
-		locale: String
+		locale: String,
+		nextAssetId: Number,
+		localesList: Array
 	},
 	components: {
-		"user-widget-textInput": textInputComponent
+		'i18n-input-widget': asyncLoadComponentI18nInputWidget,
+		"user-widget-text-input": textInputComponent
+	},
+	data() {
+		return {
+			assetId: this.nextAssetId,
+			localeLabel: "assets.textinput.placeholder." + this.assetId
+		}
+	},
+	mounted() {
+		this.$emit('set-prop', 'localeLabel', this.localeLabel);
 	}
 }
--- shared/public/components/UserWidgetTextInput.js
+++ shared/public/components/UserWidgetTextInput.js
@@ -3,6 +3,29 @@ import {template} from "./UserWidgetTextInputTemplate.js";
 export const component = {
 	template: template,
 	props: {
-		placeholder: String
+		localeLabel: String,
+		locale: String,
+		localesList: Array
+	},
+	methods: {
+		getContent(){
+			if(this.locale) {
+				if( !this.$i18n.te( this.localeLabel, this.locale ) ) {
+					if( this.$i18n.te( this.localeLabel, 'en-US' ) ) {
+						return this.$i18n.t(this.localeLabel, 'en-US');
+					}
+					else {
+						for (const locale of this.localesList) {
+							if( this.$i18n.te( this.localeLabel, locale ) ) {
+								return this.$i18n.t(this.localeLabel, locale );
+							}
+						}
+					}
+				}
+				if (this.$i18n.te(this.localeLabel, this.locale))
+					return this.$i18n.t(this.localeLabel, this.locale);
+			}
+			return ""
+		}
 	}
 };

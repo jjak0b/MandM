5115339522aeb902ea4b79e4ef01bad4c46b72e8
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-10-23 11:36:20 +0200

    [Editor/AssetsManagerRemote Edit] Allow to delete remote assets and inform user of the operation result. if an asset is a dependency of a story a list of stories that use that asset will be shown

--- editor/public/components/AssetsManagerWidgets/AssetsManagerRemoteEditFormTemplate.js
+++ editor/public/components/AssetsManagerWidgets/AssetsManagerRemoteEditFormTemplate.js
@@ -11,6 +11,7 @@ export const template =
 		<b-input-group class="mt-3">
 			<b-input-group-prepend>
 				<assets-manager-browser
+					ref="assetsBrowser"
 					id="assets-manager-widget-browser-delete"
 					v-model="selectedAsset"
 					button-only="true"
@@ -25,12 +26,52 @@ export const template =
 			></b-form-input>
 		</b-input-group>
 	</b-form-group>
-	<b-button-group>
+	<b-button-group
+		class="mb-2"
+	>
 		<b-button
-			disabled="!canDelete"
+			v-bind:disabled="!canDelete"
+			v-on:click="deleteAsset"
 			variant="danger"
 			v-t="'shared.label-delete'"
 		></b-button>
 	</b-button-group>
+	<b-alert
+		class="mb-2"
+		v-bind:show="operationAlert.isVisible"
+		dismissible
+		fade
+		v-bind:variant="operationAlert.isError ? 'danger' : 'success'"
+		v-on:dismissed="clearOperationStatus()"
+	>
+		<h4 class="alert-heading">{{ operationAlert.operationResult }}</h4>
+		<p v-if="operationAlert.operationResultCause"
+		>{{ operationAlert.operationResultCause }}</p>
+		<div v-if="operationAlert.operationMessage"
+			id="assets-manager-widget-operation-status-message"
+		>
+			<hr>
+			<p class="mb-0">{{ operationAlert.operationMessage }}</p>
+		</div>
+		<div v-if="operationAlert.operationList && operationAlert.operationList.length">
+			<p class="mb-0" >
+				<b-list-group
+					aria-describedby="assets-manager-widget-operation-status-message"
+				>
+					<b-list-group-item 
+						v-for="(item, index) in operationAlert.operationList"
+						v-bind:key="index"
+						v-bind:variant="operationAlert.isError ? 'danger' : 'success'"
+						class="d-flex justify-content-between align-items-center"
+					>{{ item.key }}
+						<b-badge v-if="item.value"
+							v-bind:variant="operationAlert.isError ? 'danger' : 'success'"
+							pill
+						>{{ item.value }}</b-badge>
+					</b-list-group-item>
+				</b-list-group>
+			</p>
+		</div>
+	</b-alert>
 </b-form>
 `
--- editor/public/components/AssetsManagerWidgets/AssetsManagerRemoteEditFormWidget.js
+++ editor/public/components/AssetsManagerWidgets/AssetsManagerRemoteEditFormWidget.js
@@ -19,22 +19,75 @@ export const component = {
 				}
 			},
 			selectedAsset: null,
-			options: null,
-			assets: []
+			options: [],
+			assets: [],
+			operationAlert: {
+				isVisible: false,
+				isError: false,
+				operationResult: null,
+				operationResultCause: null,
+				operationMessage: null,
+				operationList: null,
+			}
 		}
 	},
-	watch: {
-		selectedAsset: function (asset) {
-			this.options = null;
+	computed: {
+		canDelete() {
+			return !!this.selectedAsset;
+		}
+	},
+	methods: {
+		deleteAsset() {
+			this.clearOperationStatus();
 			let self = this;
-			$.ajax(asset.getURL(), {method: "options"})
-				.done(function (data, textStatus, request) {
-					self.options = request.getResponseHeader('Allow').split(',');
-					console.log(self.options);
+			this.selectedAsset.delete()
+				.then( (data) => {
+					this.selectedAsset = null;
+					if( self.$refs.assetsBrowser ) {
+						self.$refs.assetsBrowser.updateList();
+					}
+					self.operationAlert.operationResult = this.$t("shared.label-operation-success");
+					self.operationAlert.isError = false;
+					self.operationAlert.isVisible = true;
 				})
-				.fail(function () {
+				.catch( (xhr, textStatus, error) => {
+					console.error( "[AssetManagerRemoteEdit]", "Unable to delete asset", self.selectedAsset, "cause: ", error );
 
+					let cause = null;
+					switch( xhr.status ) {
+						case 403: // forbidden
+							self.operationAlert.operationResultCause = this.$t("AssetManager.error.label-asset-is-dependency");
+							self.operationAlert.operationMessage = this.$tc("AssetManager.label-stories-that-depend-by-asset", this.selectedAsset );
+							let dependencies = xhr.responseJSON;
+							self.operationAlert.operationList = [];
+							dependencies.forEach( (dependency) => {
+								let item = {
+									key: dependency.story,
+									value: dependency.count,
+								};
+
+								self.operationAlert.operationList.push( item );
+							});
+							break;
+						case 404: // not found (shouldn't happen)
+							self.operationAlert.operationResultCause = this.$t("AssetManager.error.label-asset-already-deleted");
+							break;
+						default:
+							self.operationAlert.operationResultCause = this.$tc("AssetManager.error.label-unable-to-delete-asset", this.selectedAsset );
+							break;
+					}
+					self.operationAlert.operationResult = this.$t("shared.label-operation-failed");
+					self.operationAlert.isError = true;
+					self.operationAlert.isVisible = true;
 				});
+		},
+		clearOperationStatus() {
+			this.operationAlert.isVisible = false;
+			this.operationAlert.isError = false;
+			this.operationAlert.operationResult = null;
+			this.operationAlert.operationResultCause = null;
+			this.operationAlert.operationMessage = null;
+			this.operationAlert.operationList = null;
 		}
 	}
 };

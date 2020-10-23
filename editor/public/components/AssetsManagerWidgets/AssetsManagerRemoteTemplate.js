9f3aeae3be5e2b5b0c9766419f87d3ce7db2bfc6
jjak0b <32840785+jjak0b@users.noreply.github.com>
2020-10-23 13:54:57 +0200

    [Editor/AssetsManagerRemote] Add new accordion UI to remote section

--- editor/public/components/AssetsManagerWidgets/AssetsManagerRemoteTemplate.js
+++ editor/public/components/AssetsManagerWidgets/AssetsManagerRemoteTemplate.js
@@ -1,22 +1,58 @@
 export const template =
 `
-<div>
-	<b-tabs fill>
-		<b-tab
-			v-bind:title="$t('shared.label-upload')"
-		>
-			<assets-manager-upload-form
-				v-bind:asset-names="cache.assetNames"
-				v-on:fetch="fetchRemoteAssets()"
-			></assets-manager-upload-form>
-		</b-tab>
-		<b-tab
-			v-bind:title="$t('shared.label-edit')"
-		>
-			<assets-manager-edit-form
-				v-on:fetch="fetchRemoteAssets()"
-			></assets-manager-edit-form>
-		</b-tab>
-	</b-tabs>
+<div class="accordion" role="tablist">
+<b-card no-body class="mb-1">
+	<b-card-header
+		header-tag="header"
+		class="p-1"
+		role="tab"
+	>
+		<b-button
+			block
+			v-b-toggle.assets-manager-remote-accordion-upload
+			variant="info"
+		>{{ $t('shared.label-upload') }}</b-button>
+	</b-card-header>
+	<b-collapse
+		id="assets-manager-remote-accordion-upload"
+		accordion="assets-manager-operations"
+		role="tabpanel"
+	>
+		<b-card-body>
+			<b-card-text>
+				<assets-manager-upload-form
+					v-bind:asset-names="cache.assetNames"
+					v-on:fetch="fetchRemoteAssets()"
+				></assets-manager-upload-form>
+			</b-card-text>
+		</b-card-body>
+	</b-collapse>
+</b-card>
+<b-card no-body class="mb-1">
+	<b-card-header
+		header-tag="header"
+		class="p-1"
+		role="tab"
+	>
+		<b-button
+			block
+			v-b-toggle.assets-manager-remote-accordion-edit
+			variant="info"
+		>{{ $t('shared.label-edit') }}</b-button>
+	</b-card-header>
+	<b-collapse
+		id="assets-manager-remote-accordion-edit"
+		accordion="assets-manager-operations"
+		role="tabpanel"
+	>
+		<b-card-body>
+			<b-card-text>
+				<assets-manager-edit-form
+					v-on:fetch="fetchRemoteAssets()"
+				></assets-manager-edit-form>
+			</b-card-text>
+		</b-card-body>
+	</b-collapse>
+</b-card>
 </div>
-`;
+`

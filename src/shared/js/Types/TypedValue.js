b24bcd253cf23f6653165c582c711ca0a1013274
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-11-26 16:08:05 +0100

    [Frontend/shared] Add equals method to TypedValue

--- src/shared/js/Types/TypedValue.js
+++ src/shared/js/Types/TypedValue.js
@@ -1,6 +1,13 @@
 export class TypedValue {
-	constructor( unparsed ) {
+	constructor( unparsed  = { type: null, value : null} ) {
 		this.type = unparsed ? unparsed.type : null;
 		this.value = unparsed ? unparsed.value : null;
+	}
+
+	equals( typedValue ) {
+		if( typedValue instanceof TypedValue ) {
+			return this.type === typedValue.type && this.value === typedValue.value;
+		}
+		return false;
 	}
 }

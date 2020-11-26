ba3608ad5dc220e27d1741a73d659aa5fe29c3c1
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-11-26 16:12:07 +0100

    [Frontend/shared] ActivityNodeBranch doesn't assign a new instance to data if has been already parsed

--- src/shared/js/ActivityNodes/ActivityNodeBranch.js
+++ src/shared/js/ActivityNodes/ActivityNodeBranch.js
@@ -6,8 +6,12 @@ import NodeUtils from "../NodeUtils.js";
 export default class ActivityNodeBranch extends ActivityNode {
 	constructor(unparsed) {
 		super(unparsed);
+		if( unparsed ) {
+			if( unparsed.data && !(unparsed.data instanceof ActivityDataBranch) ) {
+				this.data = new ActivityDataBranch( this.data );
+			}
+		}
 
-		this.data = new ActivityDataBranch( this.data );
 	}
 }
 NodeParser.register( NodeUtils.Types.Branch, ActivityNodeBranch );

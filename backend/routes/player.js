2fa7eaa7fd8471d7ccc9a20c28c77a77e6947f27
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-12-16 11:35:06 +0100

    [Backend/Player] Fix player route

--- backend/routes/player.js
+++ backend/routes/player.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-router.get('/', express.static(path.join(__basedir, "src", "player" ) ) );
-
 router.use('/locales',
 	// first provide to middleware the path it needs
 	require("../api/locales" )( path.join(__basedir, "src", "player" ) )
 );
 
+router.get('/*', express.static(path.join(__basedir, "src", "player" ) ) );
 module.exports = router;

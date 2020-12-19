00fed9aeeb6ea3abb07010ab0563a2dd46d62542
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-12-19 17:23:08 +0100

    [Frontend/Player] Add ActivityLogger class to log activity transitions

---
+++ src/player/js/ActivityLogger.js
@@ -0,0 +1,42 @@
+import ActivityLoggedEvent from "../../shared/js/ActivityLoggedEvent";
+
+export default class ActivityLogger {
+
+	constructor( endPoint ) {
+		this.history = [];
+		this.endPoint = endPoint;
+	}
+
+
+	/**
+	 * @param missionID {string}
+	 * @param activityID {string}
+	 * @param params {Object}
+	 */
+	log( missionID, activityID, params ) {
+		this.history.push( new ActivityLoggedEvent(
+			new Date().toISOString(),
+			missionID,
+			activityID,
+			params
+		));
+		this.flush();
+	};
+
+	flush() {
+		$.ajax({
+				method: "POST",
+				url: this.endPoint,
+				contentType: "application/json",
+				data: this.history
+		})
+			.done( () => {
+				this.history.splice(0, this.history.length );
+			})
+			.fail( (  jqXHR, textStatus, errorThrown) => {
+				console.warn(`[${this.constructor.name}]`, "Failed to send recent history to remote", "cause:\n", errorThrown );
+			});
+	}
+
+
+}

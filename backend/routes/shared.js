e5c0d5cd2854f974fc865332c24e5661a30f6faa
jjak0b <jacopo.rimediotti@studio.unibo.it>
2020-12-22 22:00:30 +0100

    [Backend] Add shared/locales middleware for i18n

--- backend/routes/shared.js
+++ backend/routes/shared.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-/*
-// this should be used for shared locales
+
 router.use('/locales',
 	// first provide to middleware the path it needs
-	(req, res, next) => {
-		res.locals.locales = { path: path.join(__basedir, "src", "shared" ) }
-		next();
-	},
-	require("../api/locales" )
+	require("../api/locales" )( path.join(__basedir, "src", "shared" ) )
 );
-*/
 
-router.get( '/*', express.static(path.join(__basedir, "src", "shared" ) ) );
+router.get('/*', express.static(path.join(__basedir, "src", "shared" ) ) );
 
 module.exports = router;

bc9959fa8de5501ca80db64af6827124093b851b
Lorenzo Castelli <tuonosonante@gmail.com>
2020-12-22 17:01:00 +0100

    [SCENE] Added Preview for photo capture

---
+++ src/shared/components/UserWidgetPhoto.js
@@ -0,0 +1,12 @@
+import {template} from "./UserWidgetPhotoTemplate.js";
+
+export const component = {
+    template:template,
+    props:{
+        // valueType:Object
+        //Side:String
+        //type:File.name
+        //value:Object
+
+    }
+}

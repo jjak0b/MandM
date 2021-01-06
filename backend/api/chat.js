0bdcfe261c310ae38964f0f87d77eec75db9e47e
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-01-06 10:54:26 +0100

    [Backend] Add chat API endpoints to get or edit contacts and changes API endpoints

--- backend/api/chat.js
+++ backend/api/chat.js
@@ -10,10 +10,15 @@ const StatusCodes = require("http-status-codes").StatusCodes;
 router.use( initChat );
 // list of messages of a receiver
 router.get('/messages/', API_GET_messages );
-router.get('/messages/:receiverID', API_GET_messages );
+router.get('/:receiverID/messages/', API_GET_messages );
 // add a message to the message list of a receiver
 router.put('/messages/', API_addMessage );
-router.put('/messages/:receiverID', API_addMessage );
+router.put('/:receiverID/messages/', API_addMessage );
+
+router.get( '/contacts/', API_GET_contacts );
+router.get( '/:receiverID/contacts/', API_GET_contacts );
+router.post( '/contacts/', API_POST_addContacts );
+router.post( '/:receiverID/contacts/', API_addContacts );
 
 function initChat(req, res, next) {
 	if( req.session ) {
@@ -28,9 +33,92 @@ function initChat(req, res, next) {
 
 function initChatOnSession( session ) {
 	if( !session.chat ) {
-		session.chat = [];
+		session.chat = {
+			messages: [],
+			contacts: []
+		};
 	}
 }
+
+function API_GET_contacts( req, res ) {
+	let receiverID = req.params.receiverID;
+	let selfChat = req.session.chat;
+
+	if( receiverID ) {
+		req.sessionStore.get( receiverID, (error, session) => {
+			if( error ) {
+				res.sendStatus( StatusCodes.INTERNAL_SERVER_ERROR);
+			}
+			else if( !session) {
+				res.sendStatus( StatusCodes.NOT_FOUND );
+			}
+			else {
+				res.json( session.chat.contacts || [] );
+			}
+		});
+	}
+	else {
+		res.json( selfChat.contacts );
+	}
+}
+
+function registerContacts( chat, newContacts ) {
+	for (const newContact of newContacts) {
+		let alreadyRegistered = chat.filter( contact => contact.id === newContact.id );
+		if( alreadyRegistered.length > 0 ) {
+			alreadyRegistered.name = newContact.name;
+		}
+		else {
+			chat.contacts.push({
+				id: newContact.id,
+				name: newContact.name
+			});
+		}
+	}
+}
+
+function API_addContacts( req, res ) {
+	let receiverID = req.params.receiverID;
+	let selfChat = req.session.chat;
+	let newContacts = req.body;
+
+	if( receiverID ) {
+		req.sessionStore.get( receiverID, (error, session) => {
+			if( error ) {
+				req.sendStatus( StatusCodes.INTERNAL_SERVER_ERROR );
+			}
+			else {
+				initChatOnSession( session ); // init session if needed
+
+				if( newContacts.length > 0 ) {
+
+					registerContacts( session.chat, newContacts );
+
+					session.save( (error) => {
+						if( error ) {
+							console.error( "[api/chat]", "Error saving session", session.id, "cause:", error );
+							res.sendStatus( StatusCodes.INTERNAL_SERVER_ERROR );
+						}
+						else {
+							res.json( session.chat.contacts );
+						}
+					});
+				}
+				else {
+					res.json( session.chat.contacts );
+				}
+			}
+		});
+	}
+	// otherwise add it to self messages
+	else {
+		if( newContacts.length > 0 ) {
+			registerContacts( selfChat, newContacts );
+		}
+		res.json( selfChat.contacts );
+	}
+}
+
 function API_GET_messages( req, res ) {
 	let receiverID = req.params.receiverID;
 	let selfChat = req.session.chat;
@@ -78,7 +166,9 @@ function API_addMessage( req, res ) {
 			}
 			else {
 				initChatOnSession( session ); // init session if needed
-				session.chat.push( loggedMessage );
+
+				session.chat.messages.push( loggedMessage );
+
 				session.save( (error) => {
 					if( error ) {
 						console.error( "[api/chat]", "Error saving session", session.id, "cause:", error );

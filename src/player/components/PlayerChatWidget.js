c6757e6081766670f7012246edff3b752cb8c096
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-01-06 20:26:41 +0100

    [Frontend/Player] Add Player's chat features: Add notification on new messages received Add sending messages to REST API endpoint Add receiving periodically updated messages and chat' contacts

--- src/player/components/PlayerChatWidget.js
+++ src/player/components/PlayerChatWidget.js
@@ -9,46 +9,166 @@ export const component = {
 	data() {
 		return {
 			messages: [
-				{
-					author: "notMe",
-					body: "Hello",
-					timestamp: "2019-03-23T15:35"
-				},
-				{
-					author: "me",
-					body: "World !!",
-					timestamp: "2019-03-24T15:35"
-				},
-				{
-					author: "notMe",
-					body: "Hello2",
-					timestamp: "2019-03-25T15:35"
-				},
-				{
-					author: "me",
-					body: "World 2!!",
-					timestamp: "2019-03-26T15:35"
-				}
+
 			],
 			participants: [
-				{
-					id: "me",
-					name: "Me"
-				},
-				{
-					id: "notMe",
-					name: "Helper"
-				},
+
 			],
 			mySelf: {
-				id: "me",
-				name: "self"
-			}
+				id: "0",
+				name: this.$t( "ChatWidget.label-myself" )
+			},
+			fetchTimeout: 5 * 1000,
 		}
 	},
+	computed: {
+		placeholder() { return this.$t('ChatWidget.label-type-your-message'); }
+	},
+	created() {
+		this.initChat()
+			.catch( (error) => {
+				console.error( "[Chat]", "Unable to init chat", "cause:", error );
+			})
+	},
+	mounted() {
+
+		function fetchAndWaitLoop(timeout) {
+			this.fetchAll()
+				.finally( () => {
+					setTimeout(
+						fetchAndWaitLoop.bind( this ),
+						timeout,
+						timeout
+					);
+				})
+		}
+		const start = fetchAndWaitLoop.bind( this );
+		start( this.fetchTimeout );
+	},
 	methods: {
+		initChat() {
+			return Promise.all([
+				this.registerSelfAsContact(),
+				this.fetchAll()
+			]);
+		},
+		registerSelfAsContact() {
+			return $.get( `/player/chat/` )
+				.then(ownerData => {
+					this.mySelf.id = ownerData[ 0 ];
+					return this.mySelf;
+				})
+				.catch(error => {
+					console.error("[Chat]", "Unable to fetch chatID to detect itSelf", error);
+				})
+				.then((mySelf) => {
+					return $.ajax("/player/chat/contacts/", {
+						method: "post",
+						contentType: 'application/json',
+						data: JSON.stringify([{
+							id: mySelf.id,
+							name: "Player"
+						}])
+					});
+				})
+
+		},
 		sendMessage(message) {
+			let request = $.ajax( "/player/chat/messages/", {
+				method: "put",
+				contentType: 'application/json',
+				data: JSON.stringify( message )
+			});
+
+			this.$set( message, "status", "pending" );
 			this.messages.push( message );
+			request
+				.then( () => {
+					this.$set( message, "status", "sent" );
+				})
+				.catch( () => {
+					this.$set( message, "status", "rejected" );
+				});
+		},
+		fetchAll() {
+
+			let requestContacts = $.get( `/player/chat/contacts/` );
+			let requestMessages = $.get( `/player/chat/messages/` );
+
+			return Promise.all( [requestContacts, requestMessages] )
+				.then( (responses) => {
+					let participants = responses[ 0 ];
+					let messages = responses[ 1 ];
+					let newMessages = [];
+
+					for (const message of messages) {
+
+						if( message.type === "text" ) {
+							newMessages.push( message );
+						}
+					}
+
+					this.messages = newMessages;
+					this.participants = participants;
+				})
+				.catch( (error) => {
+					console.error( "[Chat]", "Unable to fetch contacts and messages, cause:", error );
+				});
+		},
+
+		showMessagesNotification(messages) {
+			const h = this.$createElement;
+			for (const message of messages) {
+				// Create the message
+				let vNodesMsg;
+
+				if( message.type === "text" ) {
+					vNodesMsg = h(
+						'p',
+						{class: ['text-center', 'mb-0']},
+						[
+							h('strong', message.body ),
+						]
+					);
+				}
+				else {
+					vNodesMsg = h(
+						'p',
+						{class: ['text-center', 'mb-0']},
+						[
+							h('strong', this.$t('shared.ChatWidget.label-new-message') ),
+						]
+					);
+				}
+
+				// Create the title
+				const vNodesTitle = h(
+					'p',
+					{
+						class: ['d-flex', 'flex-grow-1', 'align-items-baseline', 'mr-2'],
+						"aria-label": this.$t( "shared.ChatWidget.label-new-message")
+					},
+					[
+						h('strong', { class: 'mr-2' }, this.$refs.chat.getAuthorName( message.author ) ),
+						h('small', { class: 'ml-auto text-italics' },
+							[
+								h( 'time', { datetime: message.timestamp}, new Date( message.timestamp).toLocaleTimeString() )
+							]
+						)
+					]
+				);
+
+				this.$bvToast.toast(
+					[vNodesMsg],
+					{
+						title: [vNodesTitle],
+						appendToast: true,
+						noAutoHide: true,
+						isStatus: true,
+						variant: "info",
+					}
+				)
+			}
 		}
 	}
 }

ab9a21ab52369d7076b1dd6249a7db4c16cf0176
Iacopo <i.porcedda2@gmail.com>
2021-01-14 15:48:11 +0100

    [Editor/frontend] Add aria-disabled to ActivityToolbarWidget

--- src/editor/components/ActivityEditorWidgets/ActivityToolbarWidgetTemplate.js
+++ src/editor/components/ActivityEditorWidgets/ActivityToolbarWidgetTemplate.js
@@ -5,28 +5,28 @@ export const template =
 		<div>{{ $t( 'shared.label-add' ) }}</div>
 	</li>
 	<li id="editToolbar" v-show="!isMission">
-		<div>{{ $t( 'shared.label-edit' ) }}</div>
+		<div v-bind:aria-disabled="isMission">{{ $t( 'shared.label-edit' ) }}</div>
 	</li>
 	<li id="removeToolbar" v-show="!isMission">
-		<div>{{ $t( 'shared.label-remove' ) }}</div>
+		<div v-bind:aria-disabled="isMission">{{ $t( 'shared.label-remove' ) }}</div>
 	</li>
 	<li id="grabToolbar" v-show="!isMission">
-		<div>{{ $t( 'shared.label-grab' ) }}</div>
+		<div v-bind:aria-disabled="isMission">{{ $t( 'shared.label-grab' ) }}</div>
 	</li>
 	<li id="dropToolbar" v-show="grabbedActivity">
-		<div>{{ $t( 'shared.label-drop' ) }}</div>
+		<div v-bind:aria-disabled="!grabbedActivity">{{ $t( 'shared.label-drop' ) }}</div>
 	</li>
 	<li id="copyToolbar" v-show="!isMission">
-		<div>{{ $t( 'shared.label-copy' ) }}</div>
+		<div v-bind:aria-disabled="isMission">{{ $t( 'shared.label-copy' ) }}</div>
 	</li>
 	<li id="pasteToolbar" v-show="copiedActivity">
-		<div>{{ $t( 'shared.label-paste' ) }}</div>
+		<div v-bind:aria-disabled="!copiedActivity">{{ $t( 'shared.label-paste' ) }}</div>
 	</li>
 	<li id="enableToolbar" v-show="!isMission && !active">
-		<div>{{ $t( 'shared.label-enable' ) }}</div>
+		<div v-bind:aria-disabled="!(!isMission && !active)">{{ $t( 'shared.label-enable' ) }}</div>
 	</li>
 	<li id="disableToolbar" v-show="!isMission && active">
-		<div>{{ $t( 'shared.label-disable' ) }}</div>
+		<div v-bind:aria-disabled="!(!isMission && active)">{{ $t( 'shared.label-disable' ) }}</div>
 	</li>
 </ul>
-`;
+`

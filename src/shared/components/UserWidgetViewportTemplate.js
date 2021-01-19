6ca4a1cd2dba40ec0314b92853d5a1ef532b692e
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-01-19 22:05:41 +0100

    [shared/frontend] little fix for UserWidgetViewport

--- src/shared/components/UserWidgetViewportTemplate.js
+++ src/shared/components/UserWidgetViewportTemplate.js
@@ -3,9 +3,10 @@ export const template =
 <component
 	:key="value.id"
 	:is="value.name"
-	v-model="value.value"
+	v-bind:value="value.value"
 	v-bind:tabindex="tabindex"
 	v-bind="value.props"
+	v-bind:id="value.props.id || $attrs.id || value.id"
 	:name="value.name"
 	:locale="locale"
 	:localeLabel="value.i18nCategory"

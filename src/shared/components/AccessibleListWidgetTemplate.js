f02ad11824aa2983466606375b47f4e9907e342e
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-01-26 19:59:31 +0100

    [Editor/frontend] Fix AccessibleListWidget tooltip issue in top or bottom, using right positioning; Add label as same as titles to toolbars buttons

--- src/shared/components/AccessibleListWidgetTemplate.js
+++ src/shared/components/AccessibleListWidgetTemplate.js
@@ -49,8 +49,9 @@ export const template =
 					<b-button
 						v-bind:tabindex="isFocused ? 0 : -1"
 						variant="transparent"
-						v-b-tooltip.hover
+						v-b-tooltip.right
 						v-bind:title="$t('shared.label-move-up')"
+						v-bind:aria-label="$t('shared.label-move-up')"
 						v-on:click.stop="$emit('move-up', index)">
 						<b-icon-arrow-up-short
 							v-bind:variant="getVariant(index)"
@@ -59,8 +60,9 @@ export const template =
 					<b-button
 						v-bind:tabindex="isFocused ? 0 : -1"
 						variant="transparent"
-						v-b-tooltip.hover
+						v-b-tooltip.right
 						v-bind:title="$t('shared.label-move-down')"
+						v-bind:aria-label="$t('shared.label-move-down')"
 						v-on:click.stop="$emit('move-down', index)">
 						<b-icon-arrow-down-short
 							v-bind:variant="getVariant(index)"
@@ -70,8 +72,9 @@ export const template =
 						v-bind:tabindex="isFocused ? 0 : -1"
 						variant="transparent"
 						v-if="copyPaste"
-						v-b-tooltip.hover
+						v-b-tooltip.right
 						v-bind:title="$t('shared.label-copy')"
+						v-bind:aria-label="$t('shared.label-copy')"
 						v-on:click.stop="$emit('copy', index)">
 						<b-icon-clipboard-plus
 							v-bind:variant="getVariant(index)"
@@ -81,8 +84,9 @@ export const template =
 						v-bind:tabindex="isFocused ? 0 : -1"
 						variant="transparent"
 						v-if="copyPaste"
-						v-b-tooltip.hover
+						v-b-tooltip.right
 						v-bind:title="$t('shared.label-paste')"
+						v-bind:aria-label="$t('shared.label-paste')"
 						v-on:click.stop="$emit('paste', index)">
 						<b-icon-clipboard-check
 							v-bind:variant="getVariant(index)"
@@ -92,8 +96,9 @@ export const template =
 						v-bind:tabindex="isFocused ? 0 : -1"
 						v-if="disable"
 						variant="transparent"
-						v-b-tooltip.hover
+						v-b-tooltip.right
 						v-bind:title="!items[index].active ? $t('shared.label-enable') : $t('shared.label-disable')"
+						v-bind:aria-label="!items[index].active ? $t('shared.label-enable') : $t('shared.label-disable')"
 						v-on:click.stop="$emit('enable', index)">
 						<b-icon-power
 							v-bind:variant="getVariant(index)"
@@ -102,14 +107,16 @@ export const template =
 					<b-button
 						v-bind:tabindex="isFocused ? 0 : -1"
 						variant="transparent"
-						v-b-tooltip.hover
+						v-b-tooltip.right
 						v-bind:title="$t('shared.label-delete')"
+						v-bind:aria-label="$t('shared.label-delete')"
 						v-on:click.stop="$emit('delete', index)">
 						<b-icon-x
 							v-bind:variant="getVariant(index)"
 						></b-icon-x>
 					</b-button>
 				</b-button-toolbar>
+				</div>
 				</b-col>
 			</b-row>
 		</b-container>
@@ -128,15 +135,17 @@ export const template =
 			<b-button
 				v-if="copyPaste"
 				variant="transparent"
-				v-b-tooltip.hover
+				v-b-tooltip.right
 				v-bind:title="$t('shared.label-paste')"
+				v-bind:aria-label="$t('shared.label-paste')"
 				v-on:click.stop="$emit('paste', items.length-1)">
 				<b-icon-clipboard-check></b-icon-clipboard-check>
 			</b-button>
 			<b-button
 				variant="transparent"
-				v-b-tooltip.hover
+				v-b-tooltip.right
 				v-bind:title="$t('shared.label-add')"
+				v-bind:aria-label="$t('shared.label-add')"
 				v-on:click.stop="$emit('add')">
 				<b-icon-plus></b-icon-plus>
 			</b-button>

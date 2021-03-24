a52e49aab5e4fba2d28f92ec6128d4386787b453
jjak0b <jacopo.rimediotti@studio.unibo.it>
2021-03-24 11:50:27 +0100

    [Backend/i18n] Improve i18n handling using mutex

--- backend/js/I18nHandler.js
+++ backend/js/I18nHandler.js
@@ -1,10 +1,11 @@
 const path = require( 'path' );
-const fs = require( 'fs' );
+const _fs = fs = require('fs');
+const mkdirp = require("mkdirp");
 const Promise = require( 'promise');
 const i18next = require("i18next");
 const i18nextFs = require( "i18next-fs-backend");
 const Deferred = require('promise-deferred');
-const {Mutex} = require("async-mutex");
+const {Mutex, withTimeout} = require("async-mutex");
 
 class I18nHandler {
 	constructor( basePath ) {
@@ -17,38 +18,49 @@ class I18nHandler {
 		this.i18next = i18next.createInstance();
 		this.i18next.use( i18nextFs );
 		this.mutex = new Mutex();
+		this.locales = [];
 
-		fs.mkdir( this.pathLocales, { mode: 0o0775 }, (error) => {
-		let supportedLngs = [];
-		if( error && error.code && error.code !== "EEXIST") {
-			throw error;
-		}
-		else {
-			fs.readdir( this.pathLocales, ( error, localesFilenames ) => {
-
-			if( !error && localesFilenames ) {
-				localesFilenames.filter((filename) => filename.endsWith(filenameExtension));
-				supportedLngs = new Array(localesFilenames.length);
-				localesFilenames.forEach(
-					(filename, i) => supportedLngs[i] = filename.substring(0, filename.length - filenameExtension.length));
-			}
-
-			console.log( "Detected languages in", this.pathLocales, supportedLngs );
-			this.locales = supportedLngs;
-
-			this.onReadyDeferred.resolve(
-				this.i18next.init({
-					// debug: true,
-					lng: 'en',
-					// fallbackLng: ["en"], // frontend handle localization codes
-					backend: {
-						loadPath: path.join( this.pathLocales, "{{lng}}.json" )
+		this.mutex
+			.acquire()
+			.then((release) => {
+				mkdirp(
+					this.pathLocales,
+					{
+						mode: 0o0775,
+						recursive: true,
+						fs: _fs // workaround to exclude brfs
 					}
-				})
-			);
-			});
-		}
-		});
+				)
+					.then( () => {
+						let supportedLngs = [];
+						fs.readdir( this.pathLocales, ( error, localesFilenames ) => {
+							if( !error && localesFilenames ) {
+								localesFilenames.filter((filename) => filename.endsWith(filenameExtension));
+								supportedLngs = new Array(localesFilenames.length);
+								localesFilenames.forEach(
+									(filename, i) => supportedLngs[i] = filename.substring(0, filename.length - filenameExtension.length));
+								console.log( "Detected languages in", this.pathLocales, supportedLngs );
+								this.locales = supportedLngs;
+							}
+						});
+					})
+					.catch( (error) => {
+						console.error("Unable to detect languages:", error);
+					})
+					.finally( () => {
+						this.onReadyDeferred.resolve(
+							this.i18next.init({
+								// debug: true,
+								lng: 'en',
+								// fallbackLng: ["en"], // frontend handle localization codes
+								backend: {
+									loadPath: path.join( this.pathLocales, "{{lng}}.json" )
+								}
+							})
+								.finally( release )
+						)
+					})
+		})
 	}
 
 	onInit() {
@@ -57,8 +69,8 @@ class I18nHandler {
 
 	getI18nCodeList() {
 		return this.onInit()
-			.then( () => Promise.resolve( this.locales ) )
-			.catch( () => Promise.resolve( this.locales ) )
+			.then( () => this.mutex.runExclusive( () => this.locales ) )
+			.catch( () => this.mutex.runExclusive( () => this.locales ) )
 	}
 
 	getJSON( langCode ) {
@@ -94,6 +106,9 @@ class I18nHandler {
 
 	addJSON( langCode, data ) {
 		return new Promise( (resolve, reject) => {
+		withTimeout( this.mutex, 10000 )
+			.acquire()
+			.then((release) => {
 			fs.writeFile(
 				path.join(this.pathLocales, `${langCode}.json`),
 				JSON.stringify(data),
@@ -103,21 +118,24 @@ class I18nHandler {
 				},
 				(err) => {
 					if (err) {
-						reject( err );
+						reject(err);
 					}
 					else {
-						if( !this.locales.includes( langCode ) ) {
-							console.log( "[I18nHandler]", "Register new locales for", langCode );
-							this.locales.push( langCode );
+						if (!this.locales.includes(langCode)) {
+							console.log("[I18nHandler]", "Register new locales for", langCode);
+							this.locales.push(langCode);
 						}
 						else {
-							console.log( "[I18nHandler]", "Update locales for", langCode );
+							console.log("[I18nHandler]", "Update locales for", langCode);
 						}
-						this.i18next.reloadResources( langCode );
+						this.i18next.reloadResources(langCode);
 
 						resolve();
 					}
+					release();
 				});
+			})
+			.catch( reject )
 		});
 	}
 
